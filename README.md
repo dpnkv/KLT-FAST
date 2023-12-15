c853b853a1f3462a71325a70c8412f97d208a148
dpnkv <82727654+dpnkv@users.noreply.github.com>
2023-12-15 23:52:58 +0200

    Add loading images, vectors and matrices
    Add loading images using OpenCV; add vectors and matrices using Eigen

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # KLT-FAST
-A C++ implementation of KLT and FAST algorithms
+A C++ implementation of Pyramidal KLT and FAST algorithms
+
+# Requirements
+OpenCV >= 4.7.0
+Eigen >= 3.4.0
+
+"img1.jpg" and "img2.jpg" should be in the directory with executable file
+"Eigen" directory should be in the directory with source file
+
+# Compiling
+cmake -S path-to-source -B path-to-build
+cd path-to-build
+make
--- klt.cpp
+++ klt.cpp
@@ -2,61 +2,38 @@
 #include <vector>
 #include <cmath>
 
+#include "opencv2/opencv.hpp"
+#include "Eigen/Dense"
 
-void print_vec(std::vector<std::vector<int>> &vec)
-{
-    for (std::vector<int> vect1D : vec)
-    {
-        for (int x : vect1D)
-        {
-            std::cout << x << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl << std::endl;
-}
 
-// void matrix_inverse(std::vector<std::vector<int>> &G);
-// void matrix_multiply(std::vector<std::vector<int>> &G, std::vector<std::vector<int>> &bk);
 
 int main()
 {
-    const int nx = 40, ny = 20; // original image size
-    const int Lm = 2; // deepest layer of pyramid
-    const int omega_x = 2, omega_y = 2; // 
-    const int K = 10; // number of iterations for iterative L-K
+	// pyramid: std::vector<cv::Mat> for I and J
+	cv::Mat img_i = cv::imread("img1.jpg", cv::IMREAD_GRAYSCALE);
+	cv::Mat img_j = cv::imread("img2.jpg", cv::IMREAD_GRAYSCALE);
+
+
+	// tracking point
+  	cv::Point2i p(std::floor(img_i.cols/2), std::floor(img_i.rows/2));
+
+
+    int Lm = 2; // deepest layer of pyramid
+    int omega_x = 2, omega_y = 2;
+    int K = 10; // number of iterations for iterative L-K
 
     // vector for storing image dimensions for different layers
     // pair of [nx ny] for L-th layer: [2*L 2*L+1]
-    std::vector<int> dimensions((Lm+1) * 2);
-    dimensions[0] = nx;
-    dimensions[1] = ny;
-    for (int x : dimensions)
-        std::cout << x << " ";
+    int dimensions[(Lm+1) * 2];
+    dimensions[0] = img_i.cols;
+    dimensions[1] = img_i.rows;
     
     // vector for greyscale values of original image
-    // 3dvec[L] = std::vector<std::vector<int>>(dimensions[2*L], std::vector<int>dimensions[2*L+1])
-    // for loop for copying calculated I on step L into 3dvec[L]
-    std::vector<std::vector<std::vector<int>>> I(Lm);
-    std::vector<std::vector<std::vector<int>>> J(Lm);
+    std::vector<cv::Mat> I;
+    std::vector<cv::Mat> J;
+    I.push_back(img_i);
+    J.push_back(img_j);
  
-    // I[0] = std::vector<std::vector<int>>(dimensions[0], std::vector<int>(dimensions[1]));
-    
-    // filling I and J with random int numbers from [0, 255]
-    for (int i = 0; i < dimensions[0]; i++)
-    {
-        for (int j = 0; j < dimensions[1]; j++)
-        {
-            I[0][i][j] = rand() % 256;
-        }
-    }
-    for (int i = 0; i < dimensions[0]; i++)
-    {
-        for (int j = 0; j < dimensions[1]; j++)
-        {
-            J[0][i][j] = rand() % 256;
-        }
-    }
 
     // finding dimensions for all layers
     for (size_t L = 1; L <= Lm; L++)
@@ -64,123 +41,134 @@ int main()
         // L-th [nx ny]: [floor((nx+1)/2) floor((ny+1)/2)]
         dimensions[2*L] = std::floor( (dimensions[2*(L-1)] + 1) / 2 );
         dimensions[2*L + 1] = std::floor( (dimensions[2*(L-1) + 1] + 1) / 2 );
+
+        // making layers of right size to edit their pixels later
+        I.push_back(cv::Mat(dimensions[2*L], dimensions[2*L + 1], CV_8UC1)); 
+        J.push_back(cv::Mat(dimensions[2*L], dimensions[2*L + 1], CV_8UC1)); 
     }
 
 
     // building pyramid layers
-    // second variables in loops are for edge cases (2x-1 < 0,...)
     for (int L = 1; L <= Lm; L++)
     {
-        I[L] = std::vector<std::vector<int>>(dimensions[2 * L], std::vector<int>(dimensions[2 * L + 1]));
-
-        for (int y = 1, i = 0; y < dimensions[2 * L] - 1; y++, i++)
+        for (int y = 1; y < dimensions[2 * L]; y++)
         {
-            for (int x = 1, j = 0; x < dimensions[2 * L + 1] - 1; x++, j++)
+            for (int x = 1; x < dimensions[2 * L + 1]; x++)
             {
-                I[L][i][j] = 1/4 * I[L-1][2*x][2*y] +
-                             1/8 * (I[L-1][2*x - 1][2*y] + I[L-1][2*x + 1][2*y] + I[L-1][2*x][2*y - 1] + I[L-1][2*x][2*y + 1]) +
-                             1/16 * (I[L-1][2*x - 1][2*y - 1] + I[L-1][2*x + 1][2*y + 1] + I[L-1][2*x - 1][2*y + 1] + I[L-1][2*x + 1][2*y + 1]);
+                I[L].at<uchar>(x-1,y-1) = 1/4 * I[L-1].at<uchar>(2*x,2*y) +
+                                   		  1/8 * (I[L-1].at<uchar>(2*x-1,2*y) + I[L-1].at<uchar>(2*x+1,2*y) + I[L-1].at<uchar>(2*x,2*y-1) +
+                                        	I[L-1].at<uchar>(2*x,2*y+1)) +
+                                      	  1/16 * (I[L-1].at<uchar>(2*x-1,2*y-1) + I[L-1].at<uchar>(2*x+1,2*y+1) +
+                                        	I[L-1].at<uchar>(2*x-1,2*y+1) + I[L-1].at<uchar>(2*x+1,2*y+1));
                 
-                J[L][i][j] = 1/4 * J[L-1][2*x][2*y] +
-                             1/8 * (J[L-1][2*x - 1][2*y] + J[L-1][2*x + 1][2*y] + J[L-1][2*x][2*y - 1] + J[L-1][2*x][2*y + 1]) +
-                             1/16 * (J[L-1][2*x - 1][2*y - 1] + J[L-1][2*x + 1][2*y + 1] + J[L-1][2*x - 1][2*y + 1] + J[L-1][2*x + 1][2*y + 1]);
+                J[L].at<uchar>(x-1,y-1) = 1/4 * J[L-1].at<uchar>(2*x,2*y) +
+                                      	  1/8 * (J[L-1].at<uchar>(2*x-1,2*y) + J[L-1].at<uchar>(2*x+1,2*y) + J[L-1].at<uchar>(2*x,2*y-1) +
+                                        	J[L-1].at<uchar>(2*x,2*y+1)) +
+                                      	  1/16 * (J[L-1].at<uchar>(2*x-1,2*y-1) + J[L-1].at<uchar>(2*x+1,2*y+1) +
+                                        	J[L-1].at<uchar>(2*x-1,2*y+1) + J[L-1].at<uchar>(2*x+1,2*y+1));
             }
         }
     }
 
-    std::vector<std::vector<int>> g(Lm, std::vector<int>(2)); // pyramidal guess [0, 0]
+    std::vector<Eigen::Vector2d> g(Lm); // pyramidal guess [0, 0]
     std::vector<int> u((Lm+1) * 2); // vector for point u for all layers
-    std::vector<int> v(2); // vector for point v
-    std::vector<std::vector<int>> bk(2, std::vector<int>(1)); // image mismatch vector
-    std::vector<std::vector<int>> etak(2, std::vector<int>(1)); // guess for next iteration
-    std::vector<std::vector<int>> d(Lm, std::vector<int>(2)); // final optical flow
+    u[0] = p.x;
+    u[1] = p.y;
+    cv::Point2i v; // point v
+    Eigen::Vector2d bk;
+    bk << 0, 0; // image mismatch vector
+    Eigen::Vector2d etak; // guess for next iteration
+    std::vector<Eigen::Vector2d> d(Lm); // final optical flow
 
     for (int L = Lm; L >= 0; L--)
     {
         // finding u
-        u[2*L] = (u[2*L] + 1) / std::pow(2, L);
-        u[2*L + 1] = (u[2*L + 1] + 1) / std::pow(2, L);
+        u[2*L] = (u[0]) / std::pow(2, L);
+        u[2*L + 1] = (u[1]) / std::pow(2, L);
 
         // derivative Ix, Iy
-        std::vector<std::vector<int>> Ix(dimensions[2*L],
-                                     std::vector<int>(dimensions[2*L + 1]));
-        std::vector<std::vector<int>> Iy(dimensions[2*L],
-                                     std::vector<int>(dimensions[2*L + 1]));
-        for (int y = 1, i = 0; y < dimensions[2*L] - 1; y++, i++)
+        std::vector<std::vector<int>> Ix;
+        std::vector<std::vector<int>> Iy;
+        for (int y = p.y - omega_y; y < p.y + omega_y; y++)
         {
-            for (int x = 1, j = 0; x < dimensions[2*L + 1] - 1; x++, j++)
+            for (int x = p.x - omega_x; x < p.x + omega_x; x++)
             {
-               Ix[i][j] = (I[L][x+1][y] - I[L][x-1][y]) / 2;
-               Iy[i][j] = (I[L][x][y+1] - I[L][x][y-1]) / 2;
+               Ix[x][y] = (I[L].at<uchar>(x+1,y) - I[L].at<uchar>(x-1,y)) / 2;
+               Iy[x][y] = (I[L].at<uchar>(x,y+1) - I[L].at<uchar>(x,y-1)) / 2;
             }
         }
 
-        // patial gradient matrix
-        std::vector<std::vector<int>> G(2, std::vector<int>(2));
-        for (int y = u[2*L + 1] - omega_y; y <= u[2*L + 1] + omega_y; y++)
+        // spatial gradient matrix
+        Eigen::Matrix2d G;
+        G << 0,0,0,0;
+        for (int y = p.y - omega_y; y <= p.y + omega_y; y++)
         {
-            for (int x = u[2*L] - omega_x; x <= u[2*L] + omega_x; x++)
+            for (int x = p.x - omega_x; x <= p.x + omega_x; x++)
             {
-                G[0][0] += std::pow(Ix[x][y], 2);
-                G[0][1] += Ix[x][y] * Iy[x][y];
-                G[1][0] += Ix[x][y] * Iy[x][y];
-                G[1][1] += std::pow(Iy[x][y], 2);
+                G(0,0) += std::pow(Ix[x][y], 2);
+                G(0,1) += Ix[x][y] * Iy[x][y];
+                G(1,0) += Ix[x][y] * Iy[x][y];
+                G(1,1) += std::pow(Iy[x][y], 2);
             }
         }
         
+
+
         std::vector<std::vector<int>> deltaIk;
 
         // initialization of iterative L-K
-        std::vector<std::vector<int>> nu(K, std::vector<int>(2)); //  [0, 0]
+        std::vector<Eigen::Vector2d> nu(K);
+        nu[0] << 0,0; // [0, 0]
 
         for (int k = 1; k <= K; k++)
         {
             // image difference
-            for (int y = 1, i = 0; y < dimensions[2 * L] - 1; y++, i++)
-            {
-                for (int x = 1, j = 0; x < dimensions[2 * L + 1] - 1; x++, j++)
-                {
-                    deltaIk[x][y] = I[L][x][y] - J[L][x + g[L][0] + nu[k-1][0]][y + g[L][1] + nu[k-1][1]];
+            for (int y = p.y - omega_y; y <= p.y + omega_y; y++)
+        	{
+            	for (int x = p.x - omega_x; x <= p.x + omega_x; x++)
+            	{
+                    deltaIk[x][y] = I[L].at<uchar>(x,y) - 
+                    				J[L].at<uchar>(x + g[L](0,0) + nu[k-1](0,0), y + g[L](1,0) + nu[k-1](1,0));
                 }
             }
 
             // image mismatch vector
-            for (int y = u[2*L + 1] - omega_y; y <= u[2*L + 1] + omega_y; y++)
-            {
-                for (int x = u[2*L] - omega_x; x <= u[2*L] + omega_x; x++)
-                {
-                    bk[0][0] += deltaIk[x][y] * Ix[x][y];
-                    bk[0][0] += deltaIk[x][y] * Iy[x][y];
+            for (int y = p.y - omega_y; y <= p.y + omega_y; y++)
+        	{
+            	for (int x = p.x - omega_x; x <= p.x + omega_x; x++)
+            	{
+                    bk(0,0) += deltaIk[x][y] * Ix[x][y];
+                    bk(1,0) += deltaIk[x][y] * Iy[x][y];
                 }
             }
 
             // optical flow
-            // etak = matrix_multiply(matrix_inverse(G), bk); // matrix functions not yet implemented
-
+            etak = G.inverse() * bk;
+            
             // guess for next iteration
-            nu[k][0] = nu[k-1][0] + etak[0][0];
-            nu[k][1] = nu[k-1][1] + etak[1][0];
+            nu[k](0,0) = nu[k-1](0,0) + etak(0,0);
+            nu[k](1,0) = nu[k-1](1,0) + etak(1,0);
                         
         }
 
         // final optical flow at level L
-        d[L][0] = nu[K][0];
-        d[L][1] = nu[K][1];
+        d[L](0,0) = nu[K](0,0);
+        d[L](1,0) = nu[K](1,0);
 
         // guess for next level L-1
-        g[L-1][0] = 2 * (g[L][0] + d[L][0]);
-        g[L-1][1] = 2 * (g[L][1] + d[L][1]);
+        g[L-1](0,0) = 2 * (g[L](0,0) + d[L](0,0));
+        g[L-1](1,0) = 2 * (g[L](1,0) + d[L](1,0));
 
     }
 
     // final optical flow vector
-    std::vector<int> d_final(2);
-    d_final[0] = g[0][0] + d[0][0];
-    d_final[1] = g[0][1] + d[0][1];
+    Eigen::Vector2d d_final;
+    d_final(0,0) = g[0](0,0) + d[0](0,0);
+    d_final(1,0) = g[0](1,0) + d[0](1,0);
 
     // location of point on J
-    v[0] = u[0] + d[0][0];
-    v[1] = u[1] + d[0][1];
+    v.x = u[0] + d_final(0,0);
+    v.y = u[1] + d_final(1,0);
 
 
     return 0;
